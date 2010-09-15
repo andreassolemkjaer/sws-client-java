66c4ade4e9da98530817f9c49166e28f802c8380
paal.orby <orby@sendregning.no>
2010-09-15 06:40:23 +0000

    Added shipment values to the demo invoice

--- src/main/java/no/sws/client/SwsClientDemo.java
+++ src/main/java/no/sws/client/SwsClientDemo.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2009 Pål Orby, SendRegning AS. <http://www.balder.no/> This program is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in
+ * the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package no.sws.client;
 
 import java.math.BigDecimal;
@@ -7,12 +15,19 @@ import java.util.Map;
 
 import no.sws.balance.Balance;
 import no.sws.invoice.Invoice;
-import no.sws.invoice.InvoiceImpl;
+import no.sws.invoice.InvoiceFactory;
 import no.sws.invoice.recipient.Recipient;
-import no.sws.invoice.recipient.RecipientImpl;
+import no.sws.invoice.recipient.RecipientFactory;
+import no.sws.invoice.shipment.Shipment;
+import no.sws.invoice.shipment.ShipmentFactory;
+import no.sws.invoice.shipment.ShipmentType;
 import no.sws.recipient.RecipientCategory;
 import no.sws.salesledger.SalesledgerEntry;
 
+/**
+ * @author PŒl Orby, SendRegning AS
+ *
+ */
 public class SwsClientDemo {
 
 	public static void main(String[] args) throws Exception {
@@ -25,17 +40,19 @@ public class SwsClientDemo {
 		// create a new SendRegning Web Services client
 		SwsClient swsClient = new SwsClient(username, password);
 
-		// this appends &test=true to all calls to the server, so no invoices is
+		// this appends &test=true to all calls to the server, so no invoices are
 		// actually being sent out or printed
 		swsClient.setTest(Boolean.TRUE);
-		
+
 		// get all invoices
 		List<Invoice> invoices = swsClient.getAllInvoices();
 
 		// get invoices with invoice number 1, 5 and 9
 		invoices = swsClient.getInvoices(1, 5, 9);
 
-		// send an invoice
+		// send an invoice. As you can see, you get the invoice(s) back with all
+		// fields populated by SendRegning, like account number organization number,
+		// KID number, total amount, etc...
 		invoices = swsClient.sendInvoices(generateDemoInvoice());
 
 		// get invoices with invoice number 2 and 12 as a single PDF. This byte
@@ -51,8 +68,7 @@ public class SwsClientDemo {
 
 		// get the balance (saldo) for all of your customers. Do you owe them
 		// money, or do they owe you money.
-		Map<Integer, Balance> balanceForAllRecipients = swsClient
-				.getBalanceForAllRecipients();
+		Map<Integer, Balance> balanceForAllRecipients = swsClient.getBalanceForAllRecipients();
 
 		// get the balance (saldo) for one customer
 		Balance balance = swsClient.getBalanceForRecipient(1);
@@ -60,8 +76,7 @@ public class SwsClientDemo {
 		// get all debit and credit entries (kundereskontro) for
 		// one customer. This is a full list of all the invoices sent from you,
 		// all registered payments, credit invoices (kreditnota), etc...
-		List<SalesledgerEntry> salesledgerEntries = swsClient
-				.getSalesledgerEntries(1);
+		List<SalesledgerEntry> salesledgerEntries = swsClient.getSalesledgerEntries(1);
 
 		// get all recipients
 		List<Recipient> recipients = swsClient.getAllRecipients();
@@ -73,8 +88,7 @@ public class SwsClientDemo {
 		recipients = swsClient.findRecipientByName("Ola");
 
 		// get all recipient categories
-		List<RecipientCategory> recipientCategories = swsClient
-				.getAllRecipientCategories();
+		List<RecipientCategory> recipientCategories = swsClient.getAllRecipientCategories();
 
 		// get all recipients in the given category
 		recipients = swsClient.getAllRecipientsInCategory("Hovedmedlem");
@@ -91,7 +105,7 @@ public class SwsClientDemo {
 		List<Invoice> result = new ArrayList<Invoice>();
 
 		// create a simple demo invoice (use most of the default values)
-		Invoice demo = new InvoiceImpl();
+		Invoice demo = InvoiceFactory.getInstance().getInvoice();
 
 		// used for identifying this invoice in the response (optional)
 		demo.setClientId("myUniqueId");
@@ -100,8 +114,14 @@ public class SwsClientDemo {
 		demo.setRecipient(generateDemoRecipient());
 
 		// add an invoice line
-		demo.addInvoiceLine(new BigDecimal("1.00"), "Fakturalinje #1",
-				new BigDecimal("100.00"));
+		demo.addInvoiceLine(new BigDecimal("1.00"), "Fakturalinje #1", new BigDecimal("100.00"));
+		
+		// send the invoice by email with a copy to my accountant
+		Shipment shipment = ShipmentFactory.getInstance(ShipmentType.email);
+		shipment.addEmailAddress("kari@nordmann.no");
+		shipment.addCopyAddress("kopi@regnskap.no");
+		
+		demo.setShipment(shipment);
 
 		// add the demo invoice to the list to return. You can add as many
 		// invoices you like, the only limit is the file size of the generated
@@ -118,8 +138,10 @@ public class SwsClientDemo {
 	 */
 	private static Recipient generateDemoRecipient() {
 
-		Recipient result = new RecipientImpl();
+		// Instantiate a new Recipient
+		Recipient result = RecipientFactory.getInstance();
 
+		// set demo name and address
 		result.setRecipientNo("demo1");
 		result.setName("Ola Nordmann");
 		result.setAddress1("Drammensveien 1");
